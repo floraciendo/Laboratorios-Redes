85d4d631140bd0863bc738578b90732b0b23c250
floraciendo <florencia.ramirez@alumnos.inf.utfsm.cl>
2023-09-15 19:18:30 -0300

    Actualizacion archivos
    Mover las cosas para que quede más ordenado todo lo que subimos
    + No quería borrar tu parte porque igual es de donde sacamos todo :>

---
+++ version_1/Cliente.py
@@ -0,0 +1,60 @@
+import socket
+
+def connect4_client():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the port where the server is listening
+    server_address = ('localhost', 10000)
+    print('Connecting to {} port {}'.format(*server_address))
+    sock.connect(server_address)
+
+    try:
+        # Send data
+        message = 'play'
+        print('Sending {!r}'.format(message))
+        sock.sendall(message.encode())
+
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+        
+        while amount_received < amount_expected:
+            data = sock.recv(1024)
+            amount_received += len(data)
+            print('Received {!r}'.format(data.decode()))
+            
+        
+            if data.decode() == "player_wins":
+                print("Congratulations! You won!")
+                break
+            elif data.decode() == "computer_wins":
+                print("The computer won. Better luck next time!")
+                break
+            elif data.decode() == "draw":
+                print("It's a draw!")
+                break
+
+            #this is where we get the game state and print it to the user, and then wait for the user to make a move
+
+    finally:
+        print('Closing socket')
+        sock.close()
+
+def main():
+    while True:
+        print("1. Play")
+        print("2. Exit")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            connect4_client()
+            play_again = input("Do you want to play again? (y/n): ")
+            if play_again.lower() == "n":
+                break
+        elif choice == "2":
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
---
+++ version_1/Intermediario.py
@@ -0,0 +1,57 @@
+import socket
+
+def connect4_server():
+    # Create a TCP/IP socket
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind the socket to the port
+    server_address = ('localhost', 10000)
+    print('Starting up on {} port {}'.format(*server_address))
+    server_sock.bind(server_address)
+
+    # Listen for incoming connections
+    server_sock.listen(1)
+
+    while True:
+        # Wait for a connection
+        print('Waiting for a connection')
+        client_sock, client_address = server_sock.accept()
+        try:
+            print('Connection from', client_address)
+
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = client_sock.recv(1024)
+                print('Received {!r}'.format(data.decode()))
+                if data:
+                    if data.decode() == "end_game":
+                        # Create a UDP/IP socket
+                        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                        # Define the Connect 4 server address and port
+                        connect4_server_address = ('localhost', 20000)
+                        # Send data to the Connect 4 server
+                        sent = sock.sendto(data, connect4_server_address)
+                        print("Game ended by client")
+                        break
+                    else:
+                        # Create a UDP/IP socket
+                        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                        # Define the Connect 4 server address and port
+                        connect4_server_address = ('localhost', 20000)
+                        # Send data to the Connect 4 server
+                        sent = sock.sendto(data, connect4_server_address)
+                        # Receive response from the Connect 4 server
+                        game_state, _ = sock.recvfrom(4096)
+                        print('Game state:', game_state.decode())
+                        # Send game state back to the client
+                        print('Sending game state back to the client')
+                        client_sock.sendall(game_state)
+                else:
+                    print('No more data from', client_address)
+                    break
+                
+        finally:
+            # Clean up the connection
+            client_sock.close()
+
+if __name__ == "__main__":
+    connect4_server()
